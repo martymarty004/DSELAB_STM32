1deb23c8c704ab187331fa08eaba83a35288d122
Martino P <61474670+martymarty004@users.noreply.github.com>
2026-05-19 16:23:10 +0200

    Disable systick interrupt for testing

--- Lab7_2/Core/Src/main.c
+++ Lab7_2/Core/Src/main.c
@@ -95,7 +95,7 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   /* USER CODE BEGIN 2 */
-  SysTick_Config(SystemCoreClock / 1000);
+  //SysTick_Config(SystemCoreClock / 1000);
   volatile uint32_t i;
   /* USER CODE END 2 */
 
